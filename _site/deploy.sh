275101410a19e7ebe1f6260ec29c69d70a820555
David Czaplinski <david.czaplinski@ymail.com>
2017-05-28 20:57:22 +0200

    Automatic deploy

---
+++ _site/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+SOURCE_DIR="/home/david/Code/while-true-make-games"
+DEPLOY_DIR="/home/david/Code/while-true-make-games-page"
+
+SAVED_DIR="$(pwd)"
+
+cd $SOURCE_DIR
+jekyll build
+
+cd $DEPLOY_DIR
+rm -Rf ./*
+cp -r "$SOURCE_DIR/_site/" "./"
+git add .
+git commit -m "Automatic deploy"
+git push
+
+cd $SAVED_DIR
+
