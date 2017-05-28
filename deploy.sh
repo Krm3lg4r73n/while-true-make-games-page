9eb207b0093113da7631e58ce5d7eca0aaaafefc
David Czaplinski <david.czaplinski@ymail.com>
2017-05-28 21:02:06 +0200

    Automatic deploy

--- deploy.sh
+++ deploy.sh
@@ -5,12 +5,22 @@ DEPLOY_DIR="/home/david/Code/while-true-make-games-page"
 
 SAVED_DIR="$(pwd)"
 
+echo "=============================================="
+echo "Building jekyll page"
+echo "=============================================="
 cd $SOURCE_DIR
 jekyll build
 
+echo "=============================================="
+echo "Copy page content"
+echo "=============================================="
 cd $DEPLOY_DIR
 rm -Rf ./*
 cp -r $SOURCE_DIR/_site/* .
+
+echo "=============================================="
+echo "Push to github"
+echo "=============================================="
 git add .
 git commit -m "Automatic deploy"
 git push
